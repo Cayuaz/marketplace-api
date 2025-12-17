ae589a56acc946c9edf591972b0fdb51fba6448a
Cayuã Ronald Valentin Mendonça <cayuaronald7@gmail.com>
2025-12-16 21:02:51 -0400

    chore: create user, product and order models

--- src/seeds.ts
+++ src/seeds.ts
@@ -3,16 +3,20 @@
 import { prisma } from "./lib/prisma.js";
 
 const seed = async () => {
-  //   const user = await prisma.user.create({
-  //     data: {
-  //       name: "Pedro",
-  //       lastname: "Lucas",
-  //       role: Role.ADMIN,
-  //       email: "pedro@gmail.com",
-  //       password: "123",
-  //     },
-  //   });
-  //   console.log(user);
+  // const user = await prisma.user.create({
+  //   data: {
+  //     name: "Pedro",
+  //     lastname: "Lucas",
+  //     role: Role.ADMIN,
+  //     email: "pedro@gmail.com",
+  //     password: "123",
+  //   },
+  // });
+  // console.log(user);
+
+  const deleted = await prisma.user.delete({ where: { id: 1 } });
+  console.log(deleted);
+
   const users = await prisma.user.findMany();
   console.log(users);
 };
